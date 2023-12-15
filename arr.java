fce299964b2efd86a8bb76426f23209dc7045394
Akash1211222 <90515043+Akash1211222@users.noreply.github.com>
2023-12-15 16:07:13 +0530

    f

---
+++ arr.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class arr {
+    public static void main(String[] args) {
+        int n = 5;
+        int[] arr = new int[n];
+        arr[0] = 2;
+        arr[1] = 3;
+        arr[2] = 33;
+        arr[3] = 4;
+        arr[4] = 34;
+        System.out.println(arr[2]);
+    }
+}
+    
