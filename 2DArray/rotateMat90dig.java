7b23f39c6ca54728a08557382a4eee0ce782d071
Akash1211222 <90515043+Akash1211222@users.noreply.github.com>
2023-12-17 00:49:12 +0530

    90

---
+++ 2DArray/rotateMat90dig.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+
+public class rotateMat90dig {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n][n];
+// taking input from the scanner in arr
+for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+        arr[i][j] = sc.nextInt();
+    }
+}
+    int[][] rot90 = new int[n][n];
+    for (int i = 0; i < n; i++) {
+        
+       }
+    }
+}
