22acdb818a20b394e92fdb57fb9779614543cbee
Akash1211222 <90515043+Akash1211222@users.noreply.github.com>
2023-12-29 23:46:33 +0530

    palandrome

--- string/paland.java
+++ string/paland.java
@@ -15,7 +15,7 @@ public class paland {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = "madam";
-        palanddrom(str);
+        System.out.println(palanddrom(str));
         // StringBuilder sb = new StringBuilder(s);
         // StringBuilder sb1 = new StringBuilder(s);
 
