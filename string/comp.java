08aac1a136aca60ad08b7f741e74b00e4a9940e5
Akash1211222 <90515043+Akash1211222@users.noreply.github.com>
2023-12-29 23:46:24 +0530

    comparing 2 string using s.equals

---
+++ string/comp.java
@@ -0,0 +1,17 @@
+import java.util.*;
+package string;
+
+public class comp {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String s2 = sc.nextLine();
+
+        if (s.equals(s2)) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+    }
+}
