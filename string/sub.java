d7e90d5f71ba67bb6138a2f7e4e62ad6953316ff
Akash1211222 <90515043+Akash1211222@users.noreply.github.com>
2023-12-29 23:45:55 +0530

    substring

---
+++ string/sub.java
@@ -0,0 +1,9 @@
+package string;
+import java.util.*;
+public class sub {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println(s.substring(3,8));
+    }
+}
