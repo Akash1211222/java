0dadbb9b7323dd6894f8218f2fb47c5016517cee
Akash1211222 <90515043+Akash1211222@users.noreply.github.com>
2023-12-29 22:59:03 +0530

    Palandrome que

--- string/paland.java
+++ string/paland.java
@@ -3,34 +3,44 @@ package string;
 import java.util.Scanner;
 
 public class paland {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        StringBuilder sb = new StringBuilder(s);
-        StringBuilder sb1 = new StringBuilder(s);
-
-        int i = 0;
-        int j = sb.length() - 1;
-
-        while (i < j) {
-
-            char a = sb.charAt(i);
-            char b = sb.charAt(j);
-
-            sb.setCharAt(i, b);
-            sb.setCharAt(j, a);
-            i++;
-            j--;
+    public static boolean palanddrom(String str) {
+        for (int i = 0; i < str.length()/2; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+                return false;
+            }
             
         }
-        System.out.println(sb);
-        System.out.println(sb1);
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = "madam";
+        palanddrom(str);
+        // StringBuilder sb = new StringBuilder(s);
+        // StringBuilder sb1 = new StringBuilder(s);
+
+        // int i = 0;
+        // int j = sb.length() - 1;
+
+        // while (i < j) {
+
+        //     char a = sb.charAt(i);
+        //     char b = sb.charAt(j);
+
+        //     sb.setCharAt(i, b);
+        //     sb.setCharAt(j, a);
+        //     i++;
+        //     j--;
+            
+        // }
+        // System.out.println(sb);
+        // System.out.println(sb1);
         
-        if (sb.compareTo(sb1) == 0) {
-            System.out.println("yes palandrome");
-        } else {
-            System.out.println("NO palandrome");
-        }
+        // if (sb.compareTo(sb1) == 0) {
+        //     System.out.println("yes palandrome");
+        // } else {
+        //     System.out.println("NO palandrome");
+        // }
 
     }
 }
